64f9383d50cd5d9d50da10665ed3daae39b8a97f
b6295 <b6295@DESKTOP-T51CETC>
2021-05-06 00:39:29 +0800

    new datasql here add picture part for items page

--- database.sql
+++ database.sql
@@ -40,9 +40,12 @@ CREATE TABLE items (
 /* 
 	items example
 */
-INSERT INTO items(item_name, item_price,item_description,item_availability) VALUES ('Computer', 5000,'I am a computer, i can be use to coding!',500);
-INSERT INTO items(item_name, item_price,item_description,item_availability) VALUES ('Mobile', 8000,'I am a mobile phone , i can be use to watch animation!!',300);
-INSERT INTO items(item_name, item_price,item_description,item_availability) VALUES ('Headset', 80,'I am a Headset , i can be use to listen music!!',400);
+
+Delete from items;
+ALTER TABLE items ALTER COLUMN item_id RESTART WITH 1;
+INSERT INTO items(item_name, item_price,item_description,item_availability) VALUES ('Pizza', 50,'A dish of Italian origin, consisting of a flat round base of dough baked with a topping of tomatoes and cheese, typically with added meat, fish, or vegetables.',500);
+INSERT INTO items(item_name, item_price,item_description,item_availability) VALUES ('Hamburger', 80,'A hamburger (also burger for short) is a sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun. The patty may be pan fried, grilled, smoked or flame broiled.',300);
+INSERT INTO items(item_name, item_price,item_description,item_availability) VALUES ('Sushi', 80,'Sushi is a traditional Japanese dish of prepared vinegared rice (鮨飯, sushi-meshi), usually with some sugar and salt, accompanying a variety of ingredients',400);
 
 
 
@@ -77,9 +80,23 @@ CREATE TABLE item_picture(
 );
 
 /*
+
 	order system
 	this is filled in once an order is complete
 */
+Delete from item_picture;
+ALTER TABLE item_picture ALTER COLUMN picture_id RESTART WITH 1;
+INSERT INTO item_picture(item_id,picture_data,picture_name,picture_mimetype) VALUES (1,'https://hongkongliving.com/wp-content/uploads/2020/05/Emmer-Pizza-HK.jpg','pizza1','pizza1');
+INSERT INTO item_picture(item_id,picture_data,picture_name,picture_mimetype) VALUES (1,'https://d2d5f3568fvb9s.cloudfront.net/wp-content/uploads/2020/07/20165100/aurelien-lemasson-theobald-x00CzBt4Dfk-unsplash-960x500.jpg','pizza2','pizza2');
+INSERT INTO item_picture(item_id,picture_data,picture_name,picture_mimetype) VALUES (1,'https://www.sassyhongkong.com/wp-content/uploads/2020/01/eat-drink-best-pizza-hong-kong-500x500.jpg','pizza3','pizza3');
+
+INSERT INTO item_picture(item_id,picture_data,picture_name,picture_mimetype) VALUES (2,'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1200px-RedDot_Burger.jpg','Hamburger1','Hamburger1');
+INSERT INTO item_picture(item_id,picture_data,picture_name,picture_mimetype) VALUES (2,'https://littlesunnykitchen.com/wp-content/uploads/2019/09/Air-fryer-hamburgers-6.jpg','Hamburger2','Hamburger2');
+INSERT INTO item_picture(item_id,picture_data,picture_name,picture_mimetype) VALUES (2,'https://s01.sgp1.cdn.digitaloceanspaces.com/article/143395-pysnzzzleh-1593090551.jpg','Hamburger3','Hamburger3');
+
+INSERT INTO item_picture(item_id,picture_data,picture_name,picture_mimetype) VALUES (3,'https://www.kikkoman.com/homecook/search/recipe/img/00005163.jpg','Sushi1','Sushi1');
+INSERT INTO item_picture(item_id,picture_data,picture_name,picture_mimetype) VALUES (3,'https://cdn.asiatatler.com/asiatatler/i/hk/2018/11/06132336-story-image-105298_cover_1000x817.jpg','Sushi2','Sushi2');
+INSERT INTO item_picture(item_id,picture_data,picture_name,picture_mimetype) VALUES (3,'https://media-cdn.tripadvisor.com/media/photo-s/14/7a/a7/f9/koh-sushi.jpg','Sushi3','Sushi3');
 CREATE TABLE orders(
 	order_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
 	user_id INTEGER NOT NULL,
